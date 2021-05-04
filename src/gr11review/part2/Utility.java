895c0d1fb8927e444b794d85d062a2376eff8746
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-04 15:20:54 -0400

    Methods 1 Compleated

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,12 @@
-package gr11review.part2;
+//package gr11review.part2;
 import java.io.*;
 
 public class Utility {
 
-    
-
-    
+    public static String zipZap(String str){
+        for(char i='a';i<'z';i++){
+            str = str.replaceAll("z"+String.valueOf(i)+"p", "zp");
+        }
+        return str;
+    }
 }
