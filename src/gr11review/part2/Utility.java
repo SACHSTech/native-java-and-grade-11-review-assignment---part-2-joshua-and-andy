b6eec407aa1e248b5b1b5b072ec0eab8b7d32d48
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-04 20:11:14 -0400

    Array 1 - One Dimensional && Array 3 - One Dimensional - Two Loops Completed

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,7 +2,6 @@
 import java.io.*;
 import java.util.Scanner;
 public class Utility {
-
     public static String zipZap(String str){
         for(char i='a';i<'z';i++){
             str = str.replaceAll("z"+String.valueOf(i)+"p", "zp");
@@ -18,4 +17,39 @@ public class Utility {
         br.close();
         return word;
     }
+    public static int[] tenRun(int[] nums){
+        int multiple=0;
+        for(int i=0;i<nums.length;i++){
+            if((nums[i]%10) == 0)
+                multiple=nums[i];
+            else if(multiple>0)
+                nums[i]=multiple;
+        }
+        return nums;
+    }
+    public static boolean linearIn(int[] outer, int[] inner){
+        int found=inner.length,count=0;
+        for(int i=0;i<outer.length;i++)
+            if(count<inner.length&&outer[i]==inner[count]){
+                count++; 
+                found--;
+            } 
+        if(found==0)
+            return true;
+        return false;
+    }
+    // I wanted to use a recursive binary search but I kinda gave up
+    /*private static int binarySearch(int l, int r, int x, int comparisons, int[] num){
+	    if (r >= l){
+			int mid = l + (r - l) / 2;
+			if (num[mid] == x){
+				return comparisons;
+			}
+			if (num[mid] > x){
+				return binarySearch(l, mid - 1, x, comparisons + 1, num);
+			}
+			return binarySearch(mid + 1, r, x, comparisons + 1, num);
+		}
+		return -1;
+	}*/
 }
