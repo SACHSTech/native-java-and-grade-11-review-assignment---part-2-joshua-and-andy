141d4fb9a8d8712b488ece11f0cbc25fadd731bb
Xinan Shan <>
2021-05-09 17:58:23 +0000

    notes

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -15,12 +15,15 @@ public class Utility {
     }
   }
     public static int[] notAlone(int[] nums, int value){
+     // set the array
      int myArray[] = new int [nums.length];
+     //declear the variables
+     int count;
      if (nums.length >= 1){
             myArray[0] = nums[0];
             myArray[nums.length - 1] = nums[nums.length - 1];
     }
-    for (int count = 1; count < nums.length - 1; count++){
+    for (count = 1; count < nums.length - 1; count++){
      if (nums[count] == value && nums[count] != nums[count+1] && nums[count] != nums[count-1]){
        myArray[count] = Math.max(nums[count+1], nums[count-1]);
       }else {
@@ -30,10 +33,12 @@ public class Utility {
     return myArray;
   }
   public static boolean canBalance(int[] nums){
+    //declear the variables
     int LeftSum = 0;
     int rightSum = 0;
     int count;
     int countAlt;
+
     for (count = 0; count < nums.length; count++){
       leftSum += nums[count];
       rightSum = 0;
