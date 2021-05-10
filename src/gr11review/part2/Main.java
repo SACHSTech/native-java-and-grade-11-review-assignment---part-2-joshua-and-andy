23121a9677ebb4fe44199122c7d68f2074c4fb23
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-09 22:42:21 -0400

    Added comments and style

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,27 @@
-package gr11review.part2;
+//package gr11review.part2;
+import java.io.*;
+public class Main{
 
+    public static void main(String[] args) throws IOException{
+
+        // Methods 1
+        System.out.println(Utility.zipZap("zipXzap"));
+
+        // File IO - Read 1
+        System.out.println(Utility.longestWord("words.txt"));
+
+        // Array 1 - One Dimensional
+        int ar[]= {2, 10, 3, 4, 20, 5};
+        ar = Utility.tenRun(ar);
+        for(int i=0;i<ar.length;i++){
+            System.out.println(ar[i]);
+        }
+
+        // Array 3 - One Dimensional - Two Loops
+        int ar2[]= {1, 2, 4, 4, 6};
+        int ar3[]= {2, 3, 4};
+        System.out.println(Utility.linearIn(ar2,ar3));
+
+        Utility.pascalTri(4,5);
+    }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,25 +1,51 @@
-//package gr11review.part2;
+package gr11review.part2;
 import java.io.*;
-import java.util.Scanner;
-import java.util.Arrays;  
+
 public class Utility {
+    /**
+    * Given a string, for each ocurence of a three letter word starting
+    * with z and ending with p replace it with zp.
+    * @author: Joshua. S 
+    */
     public static String zipZap(String str){
+        // Checks for a word maching the criteria then replaces it with zp
         for(char i='a';i<'z';i++){
             str = str.replaceAll("z"+String.valueOf(i)+"p", "zp");
         }
         return str;
     }
+
+    /**
+    * Reads each line in a file and determines which word is the largest
+    * then reaturns that word.
+    * @author: Joshua. S 
+    */
     public static String longestWord(String filenametxt) throws IOException{
+        // Var declarations.
         String data,word="";
         BufferedReader br = new BufferedReader(new FileReader(filenametxt));
+
+        // Reads each line then compares the word to the largest word
+        // if it's greater it now becomes the largest word.
         while((data=br.readLine())!=null)
             if(data.length()>word.length())
                 word=data;
         br.close();
         return word;
     }
+
+    /**
+    * For every number in the array that is not a multiple of 10 it
+    * changes to the previous multiple of 10.
+    * @author: Joshua. S 
+    */
     public static int[] tenRun(int[] nums){
+        // Var declarations.
         int multiple=0;
+
+        // For each value in the array check if it's a multiple of 10 if so
+        // change "multiple" to that number. If the number is not a multiple
+        // of 10 replace it with "multiple"
         for(int i=0;i<nums.length;i++){
             if((nums[i]%10) == 0)
                 multiple=nums[i];
@@ -28,8 +54,17 @@ public class Utility {
         }
         return nums;
     }
+
+    /**
+    * Returns true or false depending on of the inner array in contained in-
+    * -side the outer array.
+    * @author: Joshua. S 
+    */
     public static boolean linearIn(int[] outer, int[] inner){
+        // Var Declarations
         int found=inner.length,count=0;
+        // Goes through the array and checks of each number exists at least
+        // once.
         for(int i=0;i<outer.length;i++)
             if(count<inner.length&&outer[i]==inner[count]){
                 count++; 
@@ -39,19 +74,33 @@ public class Utility {
             return true;
         return false;
     }
-    public static void pascalTri(int i, int j){
+
+    /**
+    * Writes the the spesified digets of pascals triangle into a file called
+    * pascalOut.txt.
+    * @author: Joshua. S 
+    */
+    public static void pascalTri(int i, int j) throws IOException{
+        // Creates buffered writer to write to file.
+        BufferedWriter bw = new BufferedWriter(new FileWriter("pascalOut.txt"));
+        // Declarations of 2D array.
         int[][] triangle = new int[j][i];
+        // Loops through each space in the array and if it's zero on x or y its set it
+        // to 1. Then looks one space behind and one space above then adds them and
+        // sets the current one space as the sum.
         for(int rcx=0;rcx<i;rcx++){
             for(int ecx=0;ecx<j;ecx++){
                 if(ecx==0||rcx==0)
                     triangle[ecx][rcx]=1;
                 else
                     triangle[ecx][rcx] = triangle[ecx-1][rcx]+triangle[ecx][rcx-1];
-                System.out.print(triangle[ecx][rcx]);
+                // Writes the array value in the file
+                bw.write(""+triangle[ecx][rcx]);
                 if(ecx!=j-1)
-                    System.out.print(",");
+                    bw.write(",");
             }
-            System.out.println("");
+            bw.newLine();
         }
+        bw.close();
     }
 }
